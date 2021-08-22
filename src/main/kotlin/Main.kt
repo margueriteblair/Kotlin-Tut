e1b6fa6e0df7b5ea15a7321d3e26d109ee5709dd
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-22 06:16:41 -0400

    boolean and comparison operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 fun main() {
-    val x = 3 * 4
+    val x = 3f / 4f
     println("The value of x is $x")
+    val amIanAdult = true
+    val amIaProgrammer = true
+    val amIanAdultProgrammer = amIanAdult && amIaProgrammer
+    println(amIanAdultProgrammer)
 
 }
