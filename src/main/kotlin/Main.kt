359b57db0caa940ce528ddbd3e617020cc48a8e8
Marguerite Blair <marguerite.blair@gmail.com>
2021-08-28 08:43:08 -0400

    uncommented

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,19 +9,19 @@ fun main() {
     val str = "Kotlin is the best language!"
     println(str.toUpperCase())
 
-//    val y = 1+1
-//    if (y == 2) {
-//        println("y is 2")
-//    } else if (y == 3) {
-//        println("y is 3")
-//    } else {
-//        println("y isn't 2 or 3")
-//    }
+    val y = 1+1
+    if (y == 2) {
+        println("y is 2")
+    } else if (y == 3) {
+        println("y is 3")
+    } else {
+        println("y isn't 2 or 3")
+    }
 
-//    val number1 = readLine() ?: "0"
-//    val number2 = readLine() ?: "0"
-//    val result = number1.toInt() + number2.toInt()
-//    println(result)
+    val number1 = readLine() ?: "0"
+    val number2 = readLine() ?: "0"
+    val result = number1.toInt() + number2.toInt()
+    println(result)
 
 //    val shoppingList = listOf<String>("Lamb", "Toilet Paper", "Cereal")
 //    println(shoppingList[0])
